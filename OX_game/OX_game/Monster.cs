53453a9d21a23aac54785e419ec158ba371a899a
SEOSIX <seotoons@gmail.com>
2024-10-08 16:01:27 +0200

    Movement Monster

--- OX_game/OX_game/Monster.cs
+++ OX_game/OX_game/Monster.cs
@@ -1,31 +1,76 @@
 using System;
-using System.Xml;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 
 namespace OX_game
 {
     public class Monster
     {
-        public string monster = "o";
         public Game game;
-        
-        public void MonsterBasePosition(string [,] tab)
+        public int x2 = 5;
+        public int y2 = 5;
+        public string monster = "o";
+
+        public void MonsterSpawn(string[,] tab)
         {
-            tab[2, 5] = monster; 
+            tab[x2, y2] = monster;
+            game.ReDraw(tab);
         }
-        
-        public void MonsterMovement(string[,]tab)
+
+        public void ControlMonster(string[,] tab)
         {
             while (true)
             {
                 if (Console.KeyAvailable)
                 {
                     if (Console.ReadKey(true).Key == ConsoleKey.UpArrow)
-                    { 
-                        tab[2, 6] = monster;
-                        Console.Clear();
+                    {
+                        if (x2 == 0)
+                        {
+                            return;
+                        }
+
+                        tab[x2, y2] = "_";
+                        x2++;
+                        tab[x2, y2] = monster;
+                    }
+                    if (Console.ReadKey(true).Key == ConsoleKey.DownArrow)
+                    {
+                        if (x2 == -1)
+                        {
+                            return;
+                        }
+
+                        tab[x2, y2] = "_";
+                        x2++;
+                        tab[x2, y2] = monster;
+                    }
+                    if (Console.ReadKey(true).Key == ConsoleKey.RightArrow)
+                    {
+                        if (y2 == 0)
+                        {
+                            return;
+                        }
+
+                        tab[x2, y2] = "_";
+                        x2++;
+                        tab[x2, y2] = monster;
+                    }
+                    if (Console.ReadKey(true).Key == ConsoleKey.LeftArrow)
+                    {
+                        if (y2 == -1)
+                        {
+                            return;
+                        }
+
+                        tab[x2, y2] = "_";
+                        x2++;
+                        tab[x2, y2] = monster;
                     }
                 }
             }
         }
+        
     }
 }
