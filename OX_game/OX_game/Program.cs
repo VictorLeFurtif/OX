c77456827a7fbeef31cef876450e1fb610c3bf5e
SEOSIX <seotoons@gmail.com>
2024-10-08 14:43:31 +0200

    Final

--- OX_game/OX_game/Program.cs
+++ OX_game/OX_game/Program.cs
@@ -8,8 +8,9 @@
             string[,]tab = algo1.CreateTab();
             Hero algo2 = new Hero();
             Monster algo = new Monster();
-            algo.MonsterBasePosition(tab);
             algo2.game = algo1;
+            algo.game = algo1;
+            algo.MonsterBasePosition(tab);
             algo2.HeroSpawn(tab);
             algo1.CreateTab();
         }
